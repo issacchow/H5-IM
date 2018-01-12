cf05e5b81b57e3e171d766a8895334892dc020c6
ZhouWeiFeng <86508962@qq.com>
2018-01-12 17:12:45 +0800

    Console.log增加锁机制

--- src/main/java/cn/isc/util/ConsoleUtil.java
+++ src/main/java/cn/isc/util/ConsoleUtil.java
@@ -7,12 +7,16 @@ public class ConsoleUtil {
 
     private static SimpleDateFormat dateTimeFormat = new SimpleDateFormat();
 
+    private static Object locker = new Object();
+
     public static void log(String msg) {
-        System.out.println();
-        Date now = new Date();
-        String s = dateTimeFormat.format(now);
-        System.out.printf("[%s]", s);
-        System.out.print(msg);
+        synchronized (locker) {
+            System.out.println();
+            Date now = new Date();
+            String s = dateTimeFormat.format(now);
+            System.out.printf("[%s]", s);
+            System.out.print(msg);
+        }
     }
 
     public static void log(String format, Object... args) {
